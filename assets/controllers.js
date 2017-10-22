7d5c6eac7c91cb996ca898828ec696da4a26d06d
Brian Stefan Jensen <bsj@novicell.dk>
2017-10-22 21:24:33 +0200

    Styling etc.

--- assets/controllers.js
+++ assets/controllers.js
@@ -7,18 +7,14 @@ var cardApp = angular.module('cardApp', []);
 cardApp.controller('CardControl', function($scope) {
   $scope.cards = [
     {
-      'title': 'Hvornår tog Brian et hovedspring ud af en tysklandsbus?',
-      'year': ' ',
-      'image': 'slotspilsner.png'
-    },
-    {
-      'title': 'Hvornår tog Brian et hovedspring ud af en tysklandsbus?',
+      'title': 'Brian taget et hovedspring ud af en tysklandsbus',
       'year': '1997',
       'image': 'slotspilsner.png'
     },
     {
-      'title': 'Verdens største løg',
-      'image': 'verdensstoersteloeg'
+      'title': 'Brian og Janni bliver gift',
+      'year': '2017',
+      'image': 'brian_janni_gift.jpg'
     }
   ];
 });
